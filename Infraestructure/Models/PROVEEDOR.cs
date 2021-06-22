cf34b8b44ba1ee4d5708725a226e2d01dd18d78e
Merlin Alberto <merlin.campos@hotmail.com>
2021-06-21 19:29:41 -0600

    Correccion de nombre de campos y lista y sus Usuarios

--- Infraestructure/Models/PROVEEDOR.cs
+++ Infraestructure/Models/PROVEEDOR.cs
@@ -14,7 +14,6 @@ namespace Infraestructure.Models
     using System.ComponentModel.DataAnnotations;
 
     [MetadataType(typeof(ProveedorMetadata))]
-    
     public partial class PROVEEDOR
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
